bc2f18501234fd15262b8ff01a6d28a66c77d9dd
Shlomo Bluth <lomibluth@gmail.com>
2021-09-05 12:51:52 +0300

    add commit

--- cypress/integration/requestsTests.js
+++ cypress/integration/requestsTests.js
@@ -2,193 +2,203 @@
 
 //run tests on requests from stylistic segmentation run
 
-let sizes = [[1000, 660]]//'iphone-x',
+const urls = new Map();
+urls.set('live',Cypress.env('LIVE_URL'))
+//urls.set('dev',Cypress.env('DEV_URL')) 
+
+const sizes= new Map();
+sizes.set('desktop',[1000, 660])
+//sizes.set('mobile','iphone-x')
 
 
+urls.forEach((urlValue,urlKey)=>{
 
-sizes.forEach((size) => {
+  sizes.forEach((sizeValue,sizeKey) => {
 
-  describe('requestsTests',()=>{
+
+    describe('requestsTests '+urlKey+' '+sizeKey,()=>{
 
     
-    beforeEach(() => {
-      cy.screenSize({size:size})
-      cy.visitpage({url:'/'})
-    })
-  
-  
-    // it('Error message for Login response with a delay of 5 minutes when clicking the run button'+
-    // ' of segment page in hebrew mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'Login',
-    //     language:'Hebrew',
-    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-    //     delaySeconds:60*5
-    //   })
-    // })
-  
-    // it('Error message for Login response with a delay of 5 minutes when clicking the run button'+
-    // ' of segment page in english mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'Login',
-    //     language:'English',
-    //     message:'Oops. Something went wrong Please try again later',
-    //     delaySeconds:60*1
-    //   })
-    // })
-  
-  
-    
-    // it('Error message for Login response with status code 500 when clicking the run button of segment page'+
-    // 'in hebrew mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'Login',
-    //     language:'Hebrew',
-    //     status:500,
-    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
-    //   })
-    // })
-  
-    // it('Error message for Login response with status code 500 when clicking the run button of segment page'+
-    // 'in english mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'Login',
-    //     language:'English',
-    //     status:500,
-    //     message:'Oops. Something went wrong Please try again later'
-    //   })
-    // })
-  
-  
-  
-    // it('Error message for UploadFile response with a delay of 5 minutes when clicking the run button'+
-    // ' of segment page in hebrew mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'UploadFile',
-    //     language:'Hebrew',
-    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-    //     delaySeconds:60*1
-    //   })
-    // })
-  
-    // it('Error message for UploadFile response with a delay of 5 minutes when clicking the run button'+
-    // ' of segment page in english mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'UploadFile',
-    //     language:'English',
-    //     message:'Oops. Something went wrong Please try again later',
-    //     delaySeconds:60*1
-    //   })
-    // })
-  
-  
-    
-    // it('Error message for UploadFile response with status code 500 when clicking the run button of segment page'+
-    // 'in hebrew mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'UploadFile',
-    //     language:'Hebrew',
-    //     status:500,
-    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
-    //   })
-    // })
-  
-    // it('Error message for UploadFile response with status code 500 when clicking the run button of segment page'+
-    // 'in english mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'UploadFile',
-    //     language:'English',
-    //     status:500,
-    //     message:'Oops. Something went wrong Please try again later'
-    //   })
-    // })
-  
-  
-    // it('Error message for GetTextLargeAndSmall response with a delay of 5 minutes when clicking the run button'+
-    // ' of segment page in hebrew mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'GetTextLargeAndSmall',
-    //     language:'Hebrew',
-    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-    //     delaySeconds:60*5
-    //   })
-    // })
-  
-    // it('Error message for GetTextLargeAndSmall response with a delay of 5 minutes when clicking the run button'+
-    // ' of segment page in english mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'GetTextLargeAndSmall',
-    //     language:'English',
-    //     message:'Oops. Something went wrong Please try again later',
-    //     delaySeconds:60*5
-    //   })
-    // })
-  
-  
-    
-    // it('Error message for GetTextLargeAndSmall response with status code 500 when clicking the run button of segment page'+
-    // 'in hebrew mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'GetTextLargeAndSmall',
-    //     language:'Hebrew',
-    //     status:500,
-    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
-    //   })
-    // })
-  
-    // it('Error message for GetTextLargeAndSmall response with status code 500 when clicking the run button of segment page'+
-    // 'in english mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'GetTextLargeAndSmall',
-    //     language:'English',
-    //     status:500,
-    //     message:'Oops. Something went wrong Please try again later'
-    //   })
-    // })
-  
-    // it('Error message for segment response with a delay of 1 minutes when clicking the run button'+
-    // ' of segment page in hebrew mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'segment',
-    //     language:'Hebrew',
-    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-    //     delaySeconds:60*2
-    //   })
-    // })
-  
-    it('Error message for segment response with a delay of 1 minutes when clicking the run button'+
-    ' of segment page in english mode',()=>{
-      cy.stylisticSegmentationRequest({
-        url:'segment',
-        language:'English',
-        message:'Oops. Something went wrong Please try again later',
-        delaySeconds:60*2
+      beforeEach(() => {
+        cy.screenSize({size:sizeValue})
+        cy.visitpage({url:urlValue})
       })
-    })
-  
+
+      // it('Error message for Login response with a delay of 5 minutes when clicking the run button'+
+      // ' of segment page in hebrew mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'Login',
+      //     language:'Hebrew',
+      //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+      //     delaySeconds:60*5
+      //   })
+      // })
     
-    // it('Error message for segment response with status code 500 when clicking the run button of segment page'+
-    // 'in hebrew mode',()=>{
-    //   cy.stylisticSegmentationRequest({
-    //     url:'segment',
-    //     language:'Hebrew',
-    //     status:500,
-    //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
-    //   })
-    // })
-  
-    it('Error message for segment response with status code 500 when clicking the run button of segment page'+
-    'in english mode',()=>{
-      cy.stylisticSegmentationRequest({
-        url:'segment',
-        language:'English',
-        status:500,
-        message:'Oops. Something went wrong Please try again later'
-      })
-    })
+      // it('Error message for Login response with a delay of 5 minutes when clicking the run button'+
+      // ' of segment page in english mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'Login',
+      //     language:'English',
+      //     message:'Oops. Something went wrong Please try again later',
+      //     delaySeconds:60*1
+      //   })
+      // })
+    
+    
       
-  })
+      // it('Error message for Login response with status code 500 when clicking the run button of segment page'+
+      // 'in hebrew mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'Login',
+      //     language:'Hebrew',
+      //     status:500,
+      //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+      //   })
+      // })
+    
+      // it('Error message for Login response with status code 500 when clicking the run button of segment page'+
+      // 'in english mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'Login',
+      //     language:'English',
+      //     status:500,
+      //     message:'Oops. Something went wrong Please try again later'
+      //   })
+      // })
+    
+    
+    
+      // it('Error message for UploadFile response with a delay of 5 minutes when clicking the run button'+
+      // ' of segment page in hebrew mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'UploadFile',
+      //     language:'Hebrew',
+      //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+      //     delaySeconds:60*1
+      //   })
+      // })
+    
+      // it('Error message for UploadFile response with a delay of 5 minutes when clicking the run button'+
+      // ' of segment page in english mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'UploadFile',
+      //     language:'English',
+      //     message:'Oops. Something went wrong Please try again later',
+      //     delaySeconds:60*1
+      //   })
+      // })
+    
+    
+      
+      // it('Error message for UploadFile response with status code 500 when clicking the run button of segment page'+
+      // 'in hebrew mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'UploadFile',
+      //     language:'Hebrew',
+      //     status:500,
+      //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+      //   })
+      // })
+    
+      // it('Error message for UploadFile response with status code 500 when clicking the run button of segment page'+
+      // 'in english mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'UploadFile',
+      //     language:'English',
+      //     status:500,
+      //     message:'Oops. Something went wrong Please try again later'
+      //   })
+      // })
+    
+    
+      // it('Error message for GetTextLargeAndSmall response with a delay of 5 minutes when clicking the run button'+
+      // ' of segment page in hebrew mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'GetTextLargeAndSmall',
+      //     language:'Hebrew',
+      //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+      //     delaySeconds:60*5
+      //   })
+      // })
+    
+      // it('Error message for GetTextLargeAndSmall response with a delay of 5 minutes when clicking the run button'+
+      // ' of segment page in english mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'GetTextLargeAndSmall',
+      //     language:'English',
+      //     message:'Oops. Something went wrong Please try again later',
+      //     delaySeconds:60*5
+      //   })
+      // })
+    
+    
+      
+      // it('Error message for GetTextLargeAndSmall response with status code 500 when clicking the run button of segment page'+
+      // 'in hebrew mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'GetTextLargeAndSmall',
+      //     language:'Hebrew',
+      //     status:500,
+      //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+      //   })
+      // })
+    
+      // it('Error message for GetTextLargeAndSmall response with status code 500 when clicking the run button of segment page'+
+      // 'in english mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'GetTextLargeAndSmall',
+      //     language:'English',
+      //     status:500,
+      //     message:'Oops. Something went wrong Please try again later'
+      //   })
+      // })
+    
+      // it('Error message for segment response with a delay of 1 minutes when clicking the run button'+
+      // ' of segment page in hebrew mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'segment',
+      //     language:'Hebrew',
+      //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+      //     delaySeconds:60*2
+      //   })
+      // })
 
+    
+  
+      it('Error message for segment response with a delay of 1 minutes when clicking the run button'+
+      ' of segment page in english mode',()=>{
+        cy.stylisticSegmentationRequest({
+          url:'segment',
+          language:'English',
+          message:'Oops. Something went wrong Please try again later',
+          delaySeconds:60*2
+        })
+      })
+    
+      
+      // it('Error message for segment response with status code 500 when clicking the run button of segment page'+
+      // 'in hebrew mode',()=>{
+      //   cy.stylisticSegmentationRequest({
+      //     url:'segment',
+      //     language:'Hebrew',
+      //     status:500,
+      //     message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+      //   })
+      // })
+    
+      it('Error message for segment response with status code 500 when clicking the run button of segment page'+
+      'in english mode',()=>{
+        cy.stylisticSegmentationRequest({
+          url:'segment',
+          language:'English',
+          status:500,
+          message:'Oops. Something went wrong Please try again later'
+        })
+      })
+  
+  
+    })      
+  })
 })
 
 
--- cypress/integration/toolTests.js
+++ cypress/integration/toolTests.js
@@ -2,30 +2,39 @@
 
 //run basic tests on stylistic segmentation
 
-let sizes = [[1000, 660]]//'iphone-x',
+const urls = new Map();
+urls.set('live',Cypress.env('LIVE_URL'))
+//urls.set('dev',Cypress.env('DEV_URL')) 
+
+const sizes= new Map();
+sizes.set('desktop',[1000, 660])
+//sizes.set('mobile','iphone-x')
 
 
+urls.forEach((urlValue,urlKey)=>{
 
-sizes.forEach((size) => {
+    sizes.forEach((sizeValue,sizeKey) => {
 
-    describe('toolTests',()=>{
-
-        beforeEach(() => {
-            cy.screenSize({size:size})
-            cy.visitpage({url:'/'})
-        })
     
+        describe('toolTests '+urlKey+' '+sizeKey,()=>{
+    
+            beforeEach(() => {
+                cy.screenSize({size:sizeValue})
+                cy.visitpage({url:urlValue})
+            })
+            
+            it('stylistic segmentation run in hebrew mode',()=>{
+                cy.setLanguageMode('Hebrew')
+                cy.stylisticSegmentationRun()
+                cy.testResults()
+            })
         
-        it('stylistic segmentation run in hebrew mode',()=>{
-            cy.setLanguageMode('Hebrew')
-            cy.stylisticSegmentationRun()
-            cy.testResults()
-        })
-    
-        it('stylistic segmentation run in english mode',()=>{
-            cy.setLanguageMode('English')
-            cy.stylisticSegmentationRun()
-            cy.testResults()
+            it('stylistic segmentation run in english mode',()=>{
+                cy.setLanguageMode('English')
+                cy.stylisticSegmentationRun()
+                cy.testResults()
+            })
+        
         })
     })
 
